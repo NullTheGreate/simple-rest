ba87f6598c347a961e4334854fc4c1b81dfca83e
null <null>
2023-02-26 21:03:41 +0530

    db config

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod config;
+mod db_setup;
 mod routes;
 
 use actix_settings::ApplySettings;
@@ -6,6 +7,8 @@ use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    db_setup::establish_db_connection();
+
     HttpServer::new(|| App::new().configure(routes::init_routes))
         .apply_settings(&config::config())
         .run()
