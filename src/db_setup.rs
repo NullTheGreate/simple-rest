86dc75ba0f19321f78129782e68b68ae96688d28
null <nelprasad1986@gmail.com>
2023-02-27 20:07:37 +0530

    logs

--- src/db_setup.rs
+++ src/db_setup.rs
@@ -3,8 +3,9 @@ use diesel::Connection;
 
 use crate::config;
 
-pub fn establish_db_connection() -> PgConnection {
+pub fn establish_db_connection() -> Result<PgConnection, diesel::ConnectionError> {
     let configs = config::application_config();
     let db_url = configs.application.db_url;
-    PgConnection::establish(&db_url).unwrap()
+    PgConnection::establish(&db_url)
+    // _or_else(|_| panic!("Error connecting to {}", db_url))
 }
