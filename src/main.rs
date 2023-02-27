96a3755ad8f7ec2a51ec8d37525d8b2f9ed2f838
null <nelprasad1986@gmail.com>
2023-02-27 20:07:37 +0530

    logs

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,19 @@ mod routes;
 
 use actix_settings::ApplySettings;
 use actix_web::{App, HttpServer};
+use log::{error, trace};
+
+const APP_TITLE: &str = "SIMPLE API";
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    db_setup::establish_db_connection();
+    trace!("Starting {}", APP_TITLE);
+
+    let establish_db_connection = db_setup::establish_db_connection();
+
+    if establish_db_connection.is_err() {
+        error!("unable to connect to db")
+    }
 
     HttpServer::new(|| App::new().configure(routes::init_routes))
         .apply_settings(&config::config())
